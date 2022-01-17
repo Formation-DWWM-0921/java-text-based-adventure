38a9d420d481d21f3ffd92b7ab54920e26c797c0
Moonpearl <romain.tello@gmail.com>
2022-01-17 17:17:23 +0100

    Update ItemTest.java

--- src/test/java/com/example/entity/ItemTest.java
+++ src/test/java/com/example/entity/ItemTest.java
@@ -9,16 +9,22 @@ public class ItemTest
     @Test
     public void testConstructors()
     {
-        Room room = new Room("truc", "bidule");
+        Room room;
         Item item;
+        room = new Room("truc", "bidule");
         item = new Item(room, "machin");
         assertEquals(room, item.getRoom());
         assertEquals("machin", item.getName());
         assertEquals(true, item.isVisible());
+        assertEquals(room.getItems().size(), 1);
+        assertEquals(room.getItems().get(0), item);
 
+        room = new Room("truc", "bidule");
         item = new Item(room, "machin", false);
         assertEquals(room, item.getRoom());
         assertEquals("machin", item.getName());
         assertEquals(false, item.isVisible());
+        assertEquals(room.getItems().size(), 1);
+        assertEquals(room.getItems().get(0), item);
     }
 }
